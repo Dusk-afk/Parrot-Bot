334d2fb67df57d673ce2cd03ada4a7b01892c987
Piyush <piyushsvps@gmail.com>
2024-01-30 19:24:47 +0530

    Moved token to .env

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
+import os
+import dotenv
+
 from services.client import Client
 from services.db import Db
 from services.parrot_cog import ParrotCog
 
-TOKEN = "OTU0MzIzODA0NDUxMTMxNDQy.GY1145.vSpktJRAef2-unRFdQI4UzmcK8H1mwwq3RhRX4"
+dotenv.load_dotenv()
+token = str(os.getenv("TOKEN"))
 
 Db()
 bot = Client(
@@ -11,5 +15,4 @@ bot = Client(
 
 bot.add_cog(ParrotCog(bot))
 
-# run the bot
-bot.run(TOKEN)
+bot.run(token)
